284284c9f84cb17ae9a7faa18dc5f1dd5222527d
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-16 01:07:34 +0600

    'cors'

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
+const users = [
+  {
+    id: "1",
+    guid: "b942f5a5-906d-435c-8219-40158394a433",
+    age: 37,
+    name: "Myra Colon",
+  },
+  {
+    id: "2",
+    guid: "47b25b10-29a5-4ff7-858f-8aa8ec962c8b",
+    age: 25,
+    name: "Payne Bowers",
+  },
+  {
+    id: "3",
+    guid: "31f4b34f-28f3-4df0-b8c7-803f8f4077f7",
+    age: 20,
+    name: "Sykes Mejia",
+  },
+  {
+    id: "4",
+    guid: "6b725c09-b2c9-4fea-b71f-412d2323c2d9",
+    age: 32,
+    name: "Mckinney Estes",
+  },
+  {
+    id: "5",
+    guid: "282f28be-c228-442d-923d-d8718400c2e1",
+    age: 39,
+    name: "Leanna Mendoza",
+  },
+  {
+    id: "6",
+    guid: "bbca093e-457e-4121-8a0e-f4bfc99326ce",
+    age: 40,
+    name: "Gonzales Love",
+  },
+];
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World started with nodemon");
+});
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  const user = users[id];
+  res.send(user);
 });
 
 app.listen(port, () => {
