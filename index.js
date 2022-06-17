5810b3b9f71a3fc741ef0ae88dccf2ef8feeec19
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-18 03:52:34 +0600

    envdot process env added

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 //user middleware
 app.use(cors());
 app.use(express.json());
 
-// user info and password
-// username: monogodbuserDipok
-// password: xzvTyBlzkpAmvMLr
-
-const uri =
-  "mongodb+srv://monogodbuserDipok:xzvTyBlzkpAmvMLr@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -34,11 +31,48 @@ async function run() {
       res.send(users);
     });
 
-    // POST User : add a new user
+    // get method: update a user
+
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    // POST User : add a new user in the server
     app.post("/user", async (req, res) => {
       const newUser = req.body;
       console.log("adding newuser", newUser);
       const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // PUT User: update a existing users data
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          name: updateUser.name,
+          email: updateUser.email,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // DELETE users from the database
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
